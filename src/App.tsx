ea220492b369a1bd0486489d1bb1989827fdf49d
Volktage <leonarossini@hotmail.com>
2025-10-05 20:28:08 -0300

    Fix for photo not appearing

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
 
       <div className="container content-foreground">
         <header className="hero">
+          {/* Use a variável importada no 'src' */}
           <img src={prysPhoto} alt="Foto de Pryscilla Miranda" className="profile-photo" />
           
           <h1>Pryscilla Faria Miranda</h1>
